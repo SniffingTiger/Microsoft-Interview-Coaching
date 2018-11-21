84faa83a2388e594c7e681f9053122a0a6345ad7
SniffingTiger <SniffingTiger@users.noreply.github.com>
2018-11-21 10:56:33 -0500

    ModifyReturnTwoAddendsMethod_Returns_Empty_Array unit test - Ensured that the method does not use the same array index twice to calculate the sum.

--- Dynamics365_code/Week 3/ReturnTwoAddends.cs
+++ Dynamics365_code/Week 3/ReturnTwoAddends.cs
@@ -10,6 +10,11 @@ namespace Dynamics365_code.Week_3
     {
         public static int[] ReturnTwoAddendsMethod(int[] inputArr, int sum)
         {
+            if (inputArr.Length == 1 || inputArr.Length == 0 || inputArr is null)
+            {
+                throw new InvalidOperationException("You must input an integer array with at least two integers in it");
+            }
+
             for (int i = 0; i < inputArr.Length; i++)
             {
                 for (int j = i+1; j < inputArr.Length; j++)
--- Dynamics365_testunits/Week3_Tests.cs
+++ Dynamics365_testunits/Week3_Tests.cs
@@ -18,5 +18,41 @@ namespace Dynamics365_testunits
             
             CollectionAssert.AreEqual(expectedResult, result);
         }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, 6, 7, 15)]
+        public void ReturnTwoAddendsMethod_Returns_Empty_Array(int arr1, int arr2, int arr3, int arr4, int arr5, int arr6, int arr7, int inputSum)
+        {
+            int[] inputArr = new int[] { arr1, arr2, arr3, arr4, arr5, arr6, arr7 };
+            int[] expectedResult = new int[] { };
+
+            int[] result = ReturnTwoAddends.ReturnTwoAddendsMethod(inputArr, inputSum);
+
+            int[] inputArr2 = new int[] { arr1, arr3 };
+            int[] result2 = ReturnTwoAddends.ReturnTwoAddendsMethod(inputArr2, 2);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+            CollectionAssert.AreEqual(expectedResult, result2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ReturnTwoAddendsMethod_Throws_InvalidOperationException_When_Array_Is_Empty()
+        {
+            int[] emptyArr = new int[] { };
+
+            ReturnTwoAddends.ReturnTwoAddendsMethod(emptyArr, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ReturnTwoAddendsMethod_Throws_InvalidOperationException_When_Array_Is_One_Length()
+        {
+            int[] oneLengthArr = new int[] { 1 };
+
+            ReturnTwoAddends.ReturnTwoAddendsMethod(oneLengthArr, 3);
+        }
     }
 }
+
+
